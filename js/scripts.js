2d80a81ef5a61427fff19fb6baadb7f97d9688fc
Sean Wilson <paintballsean@gmail.com>
2020-08-22 12:27:12 -0700

    Commit tests input function and resuluting output of a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
   $("#robogerNumberInput").submit(function(event) {
-  event.preventDefault();
-  console.log(robogerNumberInput);
+    event.preventDefault();
+    console.log(robogerNumberInput);
+    let customNumber = parseInt($("#customNumber").val());
+ 
+    console.log(customNumber);
+
+    $("span.customNumber").text(customNumber);
+  });
+});
