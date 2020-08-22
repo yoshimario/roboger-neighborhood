df5fbce2884a271184ee7c616518376def420ba7
Sean Wilson <paintballsean@gmail.com>
2020-08-22 13:01:06 -0700

    Commit breaks logic  in JavaScript for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
+// Defining Functions
+
+function usingOne (customNumber) {
+  let robogerString = customNumber.toString
+}
+
+// User Input to Output
+function robogerConvert
+// The User Experience
+
 $(document).ready(function() {
   $("#robogerNumberInput").submit(function(event) {
     event.preventDefault();
     console.log(robogerNumberInput);
     let customNumber = parseInt($("#customNumber").val());
- 
-    console.log(customNumber);
 
-    $("span.customNumber").text(customNumber);
+    
   });
 });
