1f5510eaca1d624d68b2dd91d38f258d125f4653
Sean Wilson <paintballsean@gmail.com>
2020-08-23 16:21:35 -0700

    Commit adds threee functions to define three program defined variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
 // Business Logic
+function numberIsOne (userNumber) {
+  let robogerString = userNumber.toString();
+  for (number of robogerString) {
+    if (number === "1") {
+      return true;
+    };
+  };
+  return false;
+};
 
+function numberIsTwo (userNumber) {
+  let robogerString = userNumber.toString();
+  for (number of robogerString) {
+    if (number === "2") {
+      return true;
+    };
+  };
+  return false;
+};
+
+function numberIsThree (userNumber) {
+  let robogerString = userNumber.toString();
+  for (number of robogerString) {
+    if (number === "3") {
+      return true;
+    };
+  };
+  return false;
+};
 // The User Experience
 
 $(document).ready(function() {
