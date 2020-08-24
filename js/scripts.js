c90fd98093563cec3cab2399736335394d0c07cc
Sean Wilson <paintballsean@gmail.com>
2020-08-23 21:39:21 -0700

    Defines function of roboger.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business Logic
+// Program defined respsonses
 function numberIsOne (userNumber) {
   let robogerString = userNumber.toString();
   for (number of robogerString) {
@@ -28,6 +28,25 @@ function numberIsThree (userNumber) {
   };
   return false;
 };
+
+// Business Logic
+
+function roboger (userNumber) {
+  let robogerResult = [];
+  if (numberIsOne(userNumber)) {
+    return "Beep!"
+  } else if (numberIsTwo(userNumber)) {
+    return "Boop!"
+  } else if (numberIsThree(userNumber)) {
+    return "Won't you be my neighbor!"
+  } else {
+    for (var u = 0; u<=userNumber; u++;){
+      robogerResult.push(u)
+    };
+    robogerResultString = robogerResult;
+  };
+};
+
 // The User Experience
 
 $(document).ready(function() {
