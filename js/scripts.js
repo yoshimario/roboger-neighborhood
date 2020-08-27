94a87476f8c675a56c95f2218549cf8a18989dfb
Sean Wilson <paintballsean@gmail.com>
2020-08-26 21:44:01 -0700

    Commit adds first test which any number with 3 will output Won't you be my neighbor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 // Business Logic
 
-function roboger (userNumber) {
-  let robogerResult = [];
-    for (u = 0; u<=userNumber; u++) {
-      robogerResult.push(u);
+  function roboger(userNumber) {
+    let robogerResult = [];
+
+    for(i=0; i <= userNumber; i +=1) {
+      let numberString = i.toString();
+      if (numberString.includes("3")) {
+        robogerResult.push("Won't you be my neighbor?");
+        console.log(robogerResult);
+      }
     };
     robogerResultString = robogerResult.join(", ");
     return robogerResultString;
   };
-};
 
 // The User Experience
 
@@ -17,6 +21,6 @@ $(document).ready(function() {
     event.preventDefault();
     const customNumber = parseInt($("#userNumber").val());
     $("#robogerOutput").show();
-    $("#robogerResult").append(roboger(customNumber) + "  ");
+    $("#robogerResult").text(roboger(customNumber) + "  ");
   });
 });
